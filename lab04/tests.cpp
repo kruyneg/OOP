70894f123099f8c7fb6913cb91305757c688d3de
kruyneg <kruyneg@gmail.com>
2023-11-03 21:15:59 +0300

    added README, bags fixed

--- lab04/tests.cpp
+++ lab04/tests.cpp
@@ -8,11 +8,11 @@ TEST(Group_1, basic_test_set) {
     Square<double> s(2);
     Octagon<double> o(2);
 
-    EXPECT_FALSE(t == s);
-    EXPECT_FALSE(s == o);
-    EXPECT_FALSE(t == o);
+    EXPECT_FALSE(t.operator==(s));
+    EXPECT_FALSE(s.operator==(o));
+    EXPECT_FALSE(t.operator==(o));
 
-    EXPECT_TRUE(t == t);
+    EXPECT_TRUE(t.operator==(t));
 
     EXPECT_TRUE(s.center() == std::make_pair(1.0, 1.0));
     EXPECT_EQ(o.center(), std::make_pair(1.0 + 2 * std::cos(M_PI / 4.0), 1.0 + 2 * std::cos(M_PI / 4.0)));
@@ -20,7 +20,7 @@ TEST(Group_1, basic_test_set) {
 }
 
 TEST(Group_2, basic_test_set) {
-    Triangle<float> t = {std::make_pair(0.0, 0.0), std::make_pair(1.0, 0.0), std::make_pair(1.0, 1.0)};
+    Triangle<double> t = {{0.0, 0.0}, {1.0, 0.0}, {1.0, 1.0}};
     
     EXPECT_TRUE(static_cast<double>(t) == 0.5);
     EXPECT_EQ(t.center().first, 2.0 / 3.0);
@@ -30,14 +30,14 @@ TEST(Group_3, basic_test_set) {
     Square<float> s(5);
 
     EXPECT_EQ(static_cast<double>(s), 25);
-    EXPECT_EQ(s.center(), std::make_pair(2.5, 2.5));
+    EXPECT_EQ(s.center(), std::make_pair((float)2.5, (float)2.5));
 }
 
 TEST(Group_4, basic_test_set) {
     Octagon<float> o(4);
 
-    EXPECT_EQ(static_cast<double>(o), (4 + 8 * std::cos(M_PI / 4)) * (4 + 8 * std::cos(M_PI / 4)) - 2 * (4 * std::cos(M_PI / 4) * 4 * std::cos(M_PI / 4)));
-    EXPECT_EQ(o.center(), std::make_pair(2.0 + 4 * std::cos(M_PI / 4.0), 2.0 + 4 * std::cos(M_PI / 4.0)));
+    EXPECT_EQ(static_cast<double>(o), (4 + 8 * (float)std::cos(M_PI / 4)) * (4 + 8 * (float)std::cos(M_PI / 4)) - 2 * (4 * (float)std::cos(M_PI / 4) * 4 * (float)std::cos(M_PI / 4)));
+    EXPECT_EQ(o.center(), (std::make_pair<float,float>(2.0 + 4 * std::cos(M_PI / 4.0), 2.0 + 4 * std::cos(M_PI / 4.0))));
 }
 
 int main(int argc, char **argv) {
